ffadd97797cdf8a769ae0cb6fae60b0e2644fe23
ErykEG <erykegonzalez@gmail.com>
2023-03-15 19:28:14 -0600

    Homework changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-// 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
-  let temperatureColor = 'cold'
-  let temperatureValue = '10'
-  
-// 2) Agregar las funciones para manejar los eventos de Click a los botones para que 
-//     cuando se presionen se incremente o decremente el valor de la temperatura. 
+  // 1) Reemplaza estas variables por useState, puedes utilizar los valores iniciales que ya traen las variables
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+  const [temperatureValue, setTemperatureValue] = useState(10);
 
-// 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
-//    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
+  // 2) Agregar las funciones para manejar los eventos de Click a los botones para que
+  //     cuando se presionen se incremente o decremente el valor de la temperatura.
+  function Increase() {
+    if (temperatureValue == 19) {
+      setTemperatureColor("hot");
+    }
+    if (temperatureValue < 30) {
+      setTemperatureValue(temperatureValue + 1);
+    }
+  }
 
-// Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
-// De igual manera al llegar los valores a 0 no se podrá decrementas más.
+  function Decrease() {
+    if (temperatureValue > 0) {
+      if (temperatureValue == 20) {
+        setTemperatureColor("cold");
+      }
+      setTemperatureValue(temperatureValue - 1);
+    }
+  }
+
+  // 3) Agrega una lógica para que al momento de que el valor sea >= 20 grados, la variable 'temperatureColor' cambie a 'hot'
+  //    De igual manera si la temperatura baja a <20 grados, la variable cambie a 'cold'
+
+  // Para fines prácticos, agrega una regla que evite que los valores suban arriba de 30. Es decir al llegar a 30, no se podrá incrementar más.
+  // De igual manera al llegar los valores a 0 no se podrá decrementas más.
 
   return (
     <div className="app-container">
@@ -23,8 +41,8 @@ function App() {
         </div>
       </div>
       <div className="button-container">
-        <button >+</button>
-        <button >-</button>
+        <button onClick={Increase}>+</button>
+        <button onClick={Decrease}>-</button>
       </div>
     </div>
   );
